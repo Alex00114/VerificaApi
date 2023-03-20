57be99e5232557307f43b2c1a4c7e87bc7108c89
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2023-03-20 09:00:06 +0000

    punto 4

--- birre/src/app/search/search.component.ts
+++ birre/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BirreService } from '../birre.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  query!: string;
+  results: any;
+  obsBeer!: Observable<Object>;
+  constructor(public beerService: BirreService) {}
+
+  submit(query:HTMLInputElement): void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsBeer = this.beerService.searchBeer(this.query);
+    this.obsBeer.subscribe((data) => this.results = data); 
+  }
 
 }
